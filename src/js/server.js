7f217aca2e136953f3db41ad5f19819ef177c183
Ulysses3k <ceconomoujr@hotmail.com>
2023-02-12 23:00:04 +0200

    comment server.js

--- src/js/server.js
+++ src/js/server.js
@@ -1,11 +1,8 @@
 import fs from 'fs';
-import http from 'http';
 import path from 'path';
+import http from 'http';
 
-// const port = 8000;
-// const directoryName = './src';
-
-export function fileServe(fileName, directoryName, port) {
+export function fileServe(file, directoryName, port) {
 
   const types = { html: 'text/html' };
   const root = path.normalize(path.resolve(directoryName));
@@ -13,26 +10,26 @@ export function fileServe(fileName, directoryName, port) {
   const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
 
+    // Check file is HTML
     const extension = path.extname(req.url).slice(1);
     const type = extension ? types[extension] : types.html;
     const supportedExtension = Boolean(type);
-
     if (!supportedExtension) {
       res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404: File not found');
+      res.end('404: File not HTML');
       return;
     }
 
-    // let fileName = req.url;
-    // if (req.url === '/') fileName = '/template.html';
+    // Ensure file is inside the root directory
+    let fileName = req.url;
+    if (req.url === '/') fileName = file;
     const filePath = path.join(root, fileName);
     const isPathUnderRoot = path
       .normalize(path.resolve(filePath))
       .startsWith(root);
-
     if (!isPathUnderRoot) {
       res.writeHead(404, { 'Content-Type': 'text/html' });
-      res.end('404: File not found');
+      res.end('404: File not in root directory');
       return;
     }
 
@@ -45,8 +42,6 @@ export function fileServe(fileName, directoryName, port) {
         res.end(data);
       }
     });
-
-    console.log(`${filePath}`);
   });
 
   server.listen(port, () => {
