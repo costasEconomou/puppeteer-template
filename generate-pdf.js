81e75ddee79192a3703c2e42ddd0caac57d41733
Ulysses3k <ceconomoujr@hotmail.com>
2023-02-13 02:00:18 +0200

    clean up index.js

--- generate-pdf.js
+++
@@ -1,9 +0,0 @@
-import puppeteer from 'puppeteer';
-
-(async () => {
-    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
-    const page = await browser.newPage();
-    await page.setContent('file:///mnt/c/Office/PortableApps/Documents/Office/ValenTec/open-source/node-docs/index.html', {waitUntil: 'networkidle2'});
-    await page.pdf({path: 'output.pdf', format: 'A4'});
-    await browser.close();
-})();
